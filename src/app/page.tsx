1c96e2add18e997e6a08116a6803702c549c4166
Evan Kalaw <e.c.kalaw@gmail.com>
2025-04-02 16:57:01 -0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import { NotchedBorder } from "./components/NotchedBorder";
-import PillowVoter from "./components/Pillows/PillowVoter";
+import PillowPageFirst from "./components/Pillows/PillowPageFirst";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <NotchedBorder className="h-full w-full" size="large">
-        <div className="p-10 h-full flex flex-col items-center justify-center">
-          <div className="flex flex-row gap-6">
-            <div>nav-1</div>
-            <div>nav-2</div>
-            <div>nav-3</div>
-          </div>
-          <PillowVoter />
-        </div>
-      </NotchedBorder>
+      <PillowPageFirst />
     </div>
   );
 }
