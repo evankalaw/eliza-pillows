ad1d353d85e66f7477744968c07bd68a4af17dcb
Evan Kalaw <e.c.kalaw@gmail.com>
2025-04-02 17:00:00 -0500

    add that to the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { NotchedBorder } from "./components/NotchedBorder";
-import PillowVoter from "./components/Pillows/PillowVoter";
 import PillowPageFirst from "./components/Pillows/PillowPageFirst";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <NotchedBorder className="h-full w-full" size="large">
-        <PillowPageFirst />
-      </NotchedBorder>
+      <PillowPageFirst />
     </div>
   );
 }
