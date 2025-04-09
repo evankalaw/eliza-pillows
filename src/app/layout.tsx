526b2308d20a3381a36cd8066b74ed05e2b76d3a
Evan Kalaw <e.c.kalaw@gmail.com>
2025-04-09 15:15:39 -0500

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,9 @@ export default function RootLayout({
             <PillowSiteNav />
             <main className="h-full">{children}</main>
           </BorderWrappedComponent> */}
-          <main className="h-full">{children}</main>
+          <Providers>
+            <main className="h-full">{children}</main>
+          </Providers>
         </div>
       </body>
     </html>
